f0c373d3f4fdc1281d06a96139a25444fddf3c3d
sparrowchirp <tetianapavliuchenko@gmail.com>
2026-01-14 18:27:02 +0100

    02_08

--- part02-Part02_08.NumberOfNumbers/src/main/java/NumberOfNumbers.java
+++ part02-Part02_08.NumberOfNumbers/src/main/java/NumberOfNumbers.java
@@ -5,6 +5,16 @@ public class NumberOfNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int number;
+        int sum = 0;
+        do {
+            System.out.println("Give a number:");
+            number = scanner.nextInt();
+            sum++; 
+        } while (number != 0);
+
+        sum--;
+        System.out.println("Number of numbers: " + sum);
 
     }
 }
