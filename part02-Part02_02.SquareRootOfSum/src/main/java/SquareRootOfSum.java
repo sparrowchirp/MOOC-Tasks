d51a1a295ccf9d0ce3c25fbd7a521483ec6dbd77
sparrowchirp <tetianapavliuchenko@gmail.com>
2026-01-14 16:56:11 +0100

    2-02 square root

--- part02-Part02_02.SquareRootOfSum/src/main/java/SquareRootOfSum.java
+++ part02-Part02_02.SquareRootOfSum/src/main/java/SquareRootOfSum.java
@@ -5,6 +5,12 @@ public class SquareRootOfSum {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.print("First number: ");
+        int first = scanner.nextInt();
+        System.out.print("Second number: ");
+        int second = scanner.nextInt();
+        double squareRoot = Math.sqrt(first+second);
+        System.out.println("Square root of the sum is" + squareRoot);     
 
     }
 }
