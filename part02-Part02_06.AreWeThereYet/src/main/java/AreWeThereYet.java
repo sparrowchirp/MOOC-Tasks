0f0ed34882e32c025e58016e7967d54c4f7b1ad8
sparrowchirp <tetianapavliuchenko@gmail.com>
2026-01-14 17:50:48 +0100

    02_03

--- part02-Part02_06.AreWeThereYet/src/main/java/AreWeThereYet.java
+++ part02-Part02_06.AreWeThereYet/src/main/java/AreWeThereYet.java
@@ -5,6 +5,11 @@ public class AreWeThereYet {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int number;
+        do {
+            System.out.println("Give a number:");
+            number = scanner.nextInt();
+        } while (number != 4);
 
     }
 }
