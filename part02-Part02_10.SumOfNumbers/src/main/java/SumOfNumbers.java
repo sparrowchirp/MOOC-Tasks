8a63c79a8661ac98be4eceb653e4eead604c0695
sparrowchirp <tetianapavliuchenko@gmail.com>
2026-01-14 18:44:13 +0100

    02_10, 02_12

--- part02-Part02_10.SumOfNumbers/src/main/java/SumOfNumbers.java
+++ part02-Part02_10.SumOfNumbers/src/main/java/SumOfNumbers.java
@@ -5,6 +5,19 @@ public class SumOfNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int number;
+        int sum = 0;
+        while (true) {
+            System.out.println("Give a number: ");
+            number = scanner.nextInt();
+
+            if (number == 0){
+                System.out.println("Sum of the numbers: " + sum);
+                break;
+            } else {
+                sum = sum + number;
+            }
+        }
 
     }
 }
--- part02-Part02_12.AverageOfNumbers/src/main/java/AverageOfNumbers.java
+++ part02-Part02_12.AverageOfNumbers/src/main/java/AverageOfNumbers.java
@@ -5,6 +5,24 @@ public class AverageOfNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int number;
+        int sum = 0;
+        int count = 0;
+        double average;
+        while (true) {
+            System.out.println("Give a number: ");
+            number = scanner.nextInt();
+
+            if (number == 0){
+                
+                break;
+            } else {
+                sum = sum + number;
+                count++;
+            }
+        }
+        average = (double)sum / count;
+        System.out.println("Average of the numbers: " + average);
 
     }
 }
